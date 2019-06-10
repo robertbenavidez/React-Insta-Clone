ef2b3ee70be22dff7ea1ebe236c5f094647c367a
robert-benavidez <murabitbenavidez@gmail.com>
2019-06-10 19:09:52 -0400

    started working on the SearchBar

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBar = () =>  {
+    return (
+        <div>
+            <div>
+                <p>IG</p>
+            </div>
+            <div>
+            <input type="text" placeholder="Search"/>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
