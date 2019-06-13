b3c65c80c273447689ab7f21bde254fad0132c76
robert-benavidez <murabitbenavidez@gmail.com>
2019-06-13 17:48:43 -0400

    comment sections connected

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+
+class CommentSection extends Component {
+
+    render() {
+        return (
+            <div>comments here</div>
+        )
+    }
+}
+
+export default CommentSection;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Post from './Post';
+import CommentSection from '../CommentSection/CommentSection';
 
 
 const PostContainer = props => {
     return (
         <div post-container>
             {props.posts.map(posty => <Post key={posty.imageUrl} post={posty} />)}
+            <CommentSection />
         </div>
     );
 };
